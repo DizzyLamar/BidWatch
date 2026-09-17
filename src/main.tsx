390d7d493762801a1e604fa2e12d6d47447576fe
call_me_dizzy <ddzinja@gmail.com>
2026-09-17 09:24:38 +0200

    Load SEO metadata module at application startup

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import './index.css';
 import './ui-system.css';
 import './kpi.css';
+import './seo';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
